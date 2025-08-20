90f61638d81ed4a7cfa686a71bd323d0ecea5905
Escobar Jonathan <jony.escobar14@gmail.com>
2025-08-20 14:56:04 -0300

    Scaffolding
    Se agrega la estructura del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyectoInmobiliaria_ULP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
